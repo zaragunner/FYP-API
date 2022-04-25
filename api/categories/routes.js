676f17bc9446bda7a61c851b806165a1e07e1fbb
Zara Gunner <20084586@mail.wit.ie>
2022-04-25 17:36:08 +0100

    second heroku commit

--- api/categories/routes.js
+++ api/categories/routes.js
@@ -28,7 +28,7 @@ res.status(400).json({error: {message: e.message}})
 //GET ALL Categories
 categoryRouter.get('/', async (req, res) => {
     try{
-    const categories = await categoryModel.find({site_id : req.params.site_id})
+    const categories = await categoryModel.find({site_id : req.query.site_id})
     res.send(categories)
 }
 catch(e)
