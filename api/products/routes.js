4d0130d727c1e2d711edb9af4f9b994d35691823
Zara Gunner <20084586@mail.wit.ie>
2022-04-25 16:27:50 +0100

    second heroku commit

--- api/products/routes.js
+++ api/products/routes.js
@@ -9,7 +9,7 @@ import fs from 'fs'
 
 const fileStorageEngine = multer.diskStorage({
   destination : (req, file, cb)=>{
-    cb(null, '/' )
+    cb(null, '/app/uploads' )
   },
   filename : (req, file, cb) =>{
     cb(null, file.originalname)
