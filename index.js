2018eed166f4aa1ebf1468fad9648d63c6db3884
Zara Gunner <20084586@mail.wit.ie>
2022-04-25 14:55:54 +0100

    install babel-cli as dependency

--- index.js
+++ index.js
@@ -21,25 +21,9 @@ import cors from 'cors'
 mongoose.connect(process.env.mongoDB)
 .then(()=> {
   const app = express();
-  app.use(cors({
-    origin: 'https://fyp-client-44.herokuapp.com/'
-}));
-
-app.use((req, res, next) => {
-  const allowedOrigins = ['https://fyp-client-44.herokuapp.com/'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
-app.options('*', cors())
 const port = process.env.PORT;
 app.use(express.json());
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Fourth Year Project Express API')
 })
