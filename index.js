091bf33fd17a7fb293d8af79c33be3d038dbf58b
Zara Gunner <20084586@mail.wit.ie>
2022-04-25 14:50:21 +0100

    install babel-cli as dependency

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ mongoose.connect(process.env.mongoDB)
   app.use(cors({
     origin: 'https://fyp-client-44.herokuapp.com/'
 }));
+
+app.use((req, res, next) => {
+  const allowedOrigins = ['https://fyp-client-44.herokuapp.com/'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
  
 const port = process.env.PORT;
 app.use(express.json());
